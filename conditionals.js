abfd2bc10fa97fd21fc2a8103ec14a52035159f2
MaxWong03 <madmaxmmajournal@gmail.com>
2019-06-28 03:37:28 +0000

    Practiced using conditionals to tell people about the weather

---
+++ conditionals.js
@@ -0,0 +1,20 @@
+  const raining = true;
+  const cold = false;
+  if (raining) console.log('Don\'t forget your umbrella ');
+  if (cold){
+    console.log ('make sure you pick out a scarf');
+  }else{
+    console.log('short sleeve is fine');
+  }
+  
+  console.log('now you are ready to go outside');
+
+  const temp = 12;
+
+  if (temp < 0){
+    console.log('its snowing');
+  }else if (0 < temp < 15){
+    console.log('wear a jacket');
+  }else{
+    console.log('wear whatever you want');
+  }
