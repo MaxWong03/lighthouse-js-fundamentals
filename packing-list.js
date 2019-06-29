55eb404a9850e8afbcfc67a80814db6f3d1d1434
MaxWong03 <madmaxmmajournal@gmail.com>
2019-06-29 00:54:08 +0000

    practiced using while loop to loop through an array

--- packing-list.js
+++ packing-list.js
@@ -1,3 +1,10 @@
-const packingList = ['Mac', 'Wallet', 'Clothes', 'Headphones', 'Clothes'];
+const packingList = ['Mac', 'Wallet', 'Clothes', 'Headphones', 'Gloves'];
 console.log('Stuff to pack:');
 packingList.forEach(e => console.log(e));
+
+console.log('Stuff to pack (using While loop:');
+let i = 0;
+while (i < packingList.length){
+  console.log(packingList[i]);
+  i++;
+} 
